702e37e8275b0b1ae1daf5e25d7f4e2cfc6c34b5
Tissyane <tissyane@gmail.com>
2023-01-07 12:26:42 -0300

    feat: activities - listing all activities

---
+++ src/services/activities-service/index.ts
@@ -0,0 +1,32 @@
+import enrollmentRepository from "@/repositories/enrollment-repository";
+import ticketRepository from "@/repositories/ticket-repository";
+import { notFoundError } from "@/errors";
+import { cannotListActivitiesError } from "@/errors/cannot-list-activities-error";
+import activitiesRepository from "@/repositories/activities-repository";
+
+async function listActivities(userId: number) {
+  //Tem enrollment?
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!enrollment) {
+    throw notFoundError();
+  }
+  //Tem ticket pago isOnline false e includesHotel true
+  const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+
+  if (!ticket || ticket.status === "RESERVED" || ticket.TicketType.isRemote) {
+    throw cannotListActivitiesError();
+  }
+}
+
+async function getActivities(userId: number) {
+  await listActivities(userId);
+
+  const hotels = await activitiesRepository.findActivities();
+  return hotels;
+}
+
+const activitiesService = {
+  getActivities,
+};
+
+export default activitiesService;
